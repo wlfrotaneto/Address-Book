d625e6f21205a37727ed1971f5500bbc0ac0ace6
Walter Neto <wlfrotaneto@gmail.com>
2019-06-14 03:48:24 -0300

    Initial commit

---
+++ app/src/main/java/com/example/addressbook/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.addressbook;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity implements ContactsFragment.ContactsFragmentListener, DetailFragment.DetailFragmentListener, AddEditFragment.AddEditFragmentListener {
+
+    public static final String CONTACT_URI = "contact_uri";
+
+    private ContactsFragment contactsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts_activity);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null && findViewById(R.id.fragmentContainer) != null) {
+            contactsFragment = new ContactsFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(R.id.fragmentContainer, contactsFragment);
+            transaction.commit();
+        } else {
+            contactsFragment = (ContactsFragment) getSupportFragmentManager().findFragmentById(R.id.contactsFragment);
+        }
+    }
+
+    @Override
+    public void onContactSelected(Uri contactUri) {
+        if (findViewById(R.id.fragmentContainer) != null) {
+            displayContact(contactUri, R.id.fragmentContainer);
+        } else {
+            getSupportFragmentManager().popBackStack();
+            displayContact(contactUri, R.id.rightPaneContainer);
+        }
+    }
+
+
+    @Override
+    public void onAddContact() {
+        if (findViewById(R.id.fragmentContainer) != null) {
+            displayAddEditFragment(R.id.fragmentContainer, null);
+        } else {
+            displayAddEditFragment(R.id.rightPaneContainer, null);
+        }
+    }
+
+    private void displayContact(Uri contactUri, int viewID) {
+        DetailFragment detailFragment = new DetailFragment();
+
+        // specify contact's Uri as an argument to the DetailFragment
+        Bundle arguments = new Bundle();
+        arguments.putParcelable(CONTACT_URI, contactUri);
+        detailFragment.setArguments(arguments);
+
+        // use a FragmentTransaction to display the DetailFragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(viewID, detailFragment);
+        transaction.addToBackStack(null);
+        transaction.commit(); // causes DetailFragment to display
+    }
+
+    private void displayAddEditFragment(int viewID, Uri contactUri) {
+        AddEditFragment addEditFragment = new AddEditFragment();
+
+        // if editing existing contact, provide contactUri as an argument
+        if (contactUri != null) {
+            Bundle arguments = new Bundle();
+            arguments.putParcelable(CONTACT_URI, contactUri);
+            addEditFragment.setArguments(arguments);
+        }
+
+        // use a FragmentTransaction to display the AddEditFragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(viewID, addEditFragment);
+        transaction.addToBackStack(null);
+        transaction.commit(); // causes AddEditFragment to display
+    }
+
+    @Override
+    public void onContactDeleted() {
+        getSupportFragmentManager().popBackStack();
+        contactsFragment.updateContactList(); // refresh contacts
+    }
+
+    // display the AddEditFragment to edit an existing contact
+    @Override
+    public void onEditContact(Uri contactUri) {
+        if (findViewById(R.id.fragmentContainer) != null) // phone
+            displayAddEditFragment(R.id.fragmentContainer, contactUri);
+        else // tablet
+            displayAddEditFragment(R.id.rightPaneContainer, contactUri);
+    }
+
+    @Override
+    public void onAddEditCompleted(Uri contactUri) {
+        getSupportFragmentManager().popBackStack();
+        contactsFragment.updateContactList();
+
+        if (findViewById(R.id.fragmentContainer) == null) {
+            getSupportFragmentManager().popBackStack();
+
+            displayContact(contactUri, R.id.rightPaneContainer);
+        }
+    }
+}
